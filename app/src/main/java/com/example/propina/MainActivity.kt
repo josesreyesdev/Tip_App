de9c6134edbb11c66a06aa8208096ee1b0282306
Jose SR <josesanchez09190@gmail.com>
2022-01-17 12:46:02 -0600

    Se agrego el proyecto

---
+++ app/src/main/java/com/example/propina/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.propina
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.Log.WARN
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.propina.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
+
+//Variable para probar el log
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculate.setOnClickListener { calcularPropina() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _/*keyEvent */ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
+
+        /* funciones para pruebas
+        division()
+        logging() */
+    }
+
+    private fun calcularPropina() {
+        //Get costo del servicio
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+        //Get porcentaje de propina a partir del radioButton seleccionado
+        val tipPorcentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        //Calculo de propina
+        var tip = cost * tipPorcentage
+
+        //calculo de redondeo de la propina, si fue seleccionado
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip) //ceil -> redondea la propina
+        }
+
+        displayTip(tip)
+
+    }
+
+    //Formateador de moneda
+    private fun displayTip(propina: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(propina)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    //Evento de escucha de claves para ocultar teclado cuando se presiona enter
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+    //Funcion para probar errores de consola y hacer debug
+    private fun division() {
+        val numerator = 60
+        var denominador = 4
+        repeat(5) {
+            if (denominador != 0) {
+                println(numerator / denominador)
+            }
+            denominador--
+        }
+    }
+
+    //Función para probar distintos logs en consola
+    private fun logging() {
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: Warns about potential for serious error")
+        Log.i(TAG, "INFO: Reporting technical information")
+        Log.d(TAG, "DEBUG: reporting technical information, useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+    }
+}
---
+++ app/src/test/java/com/example/propina/ExampleUnitTest.kt
@@ -0,0 +1,34 @@
+package com.example.propina
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+    @Test
+    fun generate_numbers() {
+        /*
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("Valor de rollResult no esta entre 1 and 6", rollResult in 1..6 )
+        */
+    }
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
+
+/**
+ * common asserts
+ * assertEquals(), assertNotEquals(), assertThat(), assertTrue(), assertFalse(),
+ * assertNull(), assertNotNull()
+ * more assert => https://developer.android.com/reference/junit/framework/Assert
+ * */
