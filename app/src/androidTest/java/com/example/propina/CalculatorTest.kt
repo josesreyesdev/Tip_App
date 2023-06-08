c30317b3f9e99835449a14f114765eaf12b90814
Jose SR <josesanchez09190@gmail.com>
2023-06-08 01:19:05 -0600

    update gradle and test

--- app/src/androidTest/java/com/example/propina/CalculatorTest.kt
+++ app/src/androidTest/java/com/example/propina/CalculatorTest.kt
@@ -9,12 +9,14 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.hamcrest.Matchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class CalculatorTest {
 
     @get:Rule
@@ -32,18 +34,34 @@ class CalculatorTest {
 
         onView(withId(R.id.tip_result))
             .check( matches(withText(containsString("$10.00"))))
+    }
 
+    @Test
+    fun calculate_18_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.option_eighteen_percent))
+            .perform(click())
+
+        onView(withId(R.id.round_up_switch))
+            .perform(click())
+
+        onView(withId(R.id.calculate))
+            .perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$9.00"))))
     }
 
     @Test
     fun calculate_15_percent_tip() {
         onView(withId(R.id.cost_of_service_edit_text))
             .perform( typeText("100.00"))
+            .perform(ViewActions.closeSoftKeyboard())
 
         onView(withId(R.id.option_fifteen_percent))
-            .perform( click())
-
-        onView(withId(R.id.round_up_switch))
             .perform( click())
 
         onView(withId(R.id.calculate))
